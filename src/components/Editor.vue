7d7a5dd6433c474e300be6c5c6e170bfc3c56c0f
aris-konstantinidis <aris.konstantinidis@students.unibe.ch>
2020-11-08 03:44:22 +0100

    integrate vuedraggable, not fully working yet

--- src/components/Editor.vue
+++ src/components/Editor.vue
@@ -1,19 +1,32 @@
 <template>
   <div class="row">
     <div class="col-3">
-      {{this.trials}}
+      <draggable class="dragArea list-group" :list="trials" :group="{name: 'blocks', pull: 'clone', put: false}" :clone='cloneTrial'>
+        <div class="list-group-item" v-for='trial in trials' :key="trial.id">
+          {{ trial.name }}
+        </div>
+      </draggable>
     </div>
     <div class="col-3">
-      {{this.blocks}}
+      <draggable class="dragArea list-group" :list="blocks" group="blocks">
+        <div class="list-group-item" v-for='block in blocks' :key="block.id">
+          {{ block.name }}
+        </div>
+      </draggable>
     </div>
     <div class="col-3">
-      {{this.groups}}
+      <draggable />
     </div>
   </div>
 </template>
 
 <script>
+import draggable from 'vuedraggable'
+let ids = 0;
 export default {
+  components: {
+    draggable
+  },
   computed: {
       trials() {
         return this.$store.state.trials
@@ -24,6 +37,14 @@ export default {
       groups() {
         return this.$store.state.groups
       }
+  },
+  methods: {
+    cloneTrial({id}) {
+      return {
+        id: ids++,
+        name: id
+      }
+    }
   }
 }
 </script>
--- src/main.js
+++ src/main.js
@@ -1,8 +1,10 @@
 import Vue from 'vue'
 import App from './App.vue'
 import store from './store'
+import draggable from 'vuedraggable'
 
 Vue.config.productionTip = false
+Vue.use(draggable)
 
 new Vue({
   store,
