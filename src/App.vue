8d64d9855a1d0676baca678337b14e6ef751163b
ariskonstantinidis95 <konstantinidis.aris95@gmail.com>
2020-11-12 01:01:39 +0100

    fixed top button height

--- src/App.vue
+++ src/App.vue
@@ -9,7 +9,7 @@
       <div class="collapse navbar-collapse" id="navbarNavDropdown">
         <ul class="navbar-nav ml-auto">
           <li class="nav-item mr-auto dropleft">
-            <button class="nav-link text-light btn btn-info" href="#" id="navbarDropdownMenuLink" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">New</button>
+            <button class="text-light btn btn-info" href="#" id="navbarDropdownMenuLink" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">New</button>
             <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
               <a class="dropdown-item btn text-success" data-target="#trialForm" data-toggle="modal"><svg width="1em" height="1em" viewBox="0 0 16 16" class="bi bi-box" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
   <path fill-rule="evenodd" d="M8.186 1.113a.5.5 0 0 0-.372 0L1.846 3.5 8 5.961 14.154 3.5 8.186 1.113zM15 4.239l-6.5 2.6v7.922l6.5-2.6V4.24zM7.5 14.762V6.838L1 4.239v7.923l6.5 2.6zM7.443.184a1.5 1.5 0 0 1 1.114 0l7.129 2.852A.5.5 0 0 1 16 3.5v8.662a1 1 0 0 1-.629.928l-7.185 2.874a.5.5 0 0 1-.372 0L.63 13.09a1 1 0 0 1-.63-.928V3.5a.5.5 0 0 1 .314-.464L7.443.184z"/>
@@ -23,13 +23,13 @@
             </div>
           </li>
           <li class="nav-item mr-auto ml-2">
-            <a type="button" class="nav-link btn btn-secondary" @click="fetchProtocols">Fetch</a>
+            <a type="button" class="btn btn-secondary" @click="fetchProtocols">Fetch</a>
           </li>
           <li class="nav-item mr-auto ml-2">
-            <a type="button" class="nav-link btn btn-light" @click="resetAll">Reset</a>
+            <a type="button" class="btn btn-light" @click="resetAll">Reset</a>
           </li>
           <li class="nav-item mr-auto ml-2">
-            <a type="button" class="nav-link btn btn-warning" @click="exportProtocols" data-toggle="modal" data-target="#previewModal">Export</a>
+            <a type="button" class="btn btn-warning" @click="exportProtocols" data-toggle="modal" data-target="#previewModal">Export</a>
           </li>
         </ul>
       </div>
